81552e4bfeddbac8347c804e2bb959aab365402d
Joe Pell <jpell3130@gmail.com>
2019-04-09 19:02:39 -0400

    implemented validation to check if author is answering own poll

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ApplicationRecord
   validates :user_id, :answer_id, presence: true
   validate :respondent_already_answered?
+  validate :respondent_is_author?
 
   belongs_to :answer_choice, foreign_key: :answer_id, class_name: "AnswerChoice"
   belongs_to :respondent, foreign_key: :user_id, class_name: "User"
@@ -15,6 +16,12 @@ class Response < ApplicationRecord
   def respondent_already_answered?
     if self.non_duplicate_responses.where({ user_id: self.user_id }).exists?
       self.errors["user_id"] << "User already answered this question"
+    end
+  end
+
+  def respondent_is_author?
+    if self.user_id == self.answer_choice.question.poll.user_id
+      self.errors["user_id"] << "Poll author cannot respond to questions"
     end
   end
 end
