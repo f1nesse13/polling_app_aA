bd1fad81f2281a381f9a91c9a78db90432045127
Joe Pell <jpell3130@gmail.com>
2019-04-09 12:23:58 -0400

    added validation to check for multiple answers to the same question from a single user

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,6 @@
 class Response < ApplicationRecord
   validates :user_id, :answer_id, presence: true
+  validate :respondent_already_answered?
 
   belongs_to :answer_choice, foreign_key: :answer_id, class_name: "AnswerChoice"
   belongs_to :respondent, foreign_key: :user_id, class_name: "User"
@@ -9,5 +10,11 @@ class Response < ApplicationRecord
 
   def non_duplicate_responses
     self.sibiling_responses.where.not({ id: self.id })
+  end
+
+  def respondent_already_answered?
+    if self.non_duplicate_responses.where({ user_id: self.user_id }).exists?
+      self.errors["user_id"] << "User already answered this question"
+    end
   end
 end
