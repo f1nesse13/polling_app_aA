a196342f41d97ba4888c6414344bf57c411f88af
Joe Pell <jpell3130@gmail.com>
2019-04-09 23:09:26 -0400

    refactored result method

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,14 @@ class Question < ApplicationRecord
 
   has_many :answer_choices, foreign_key: :question_id, class_name: "AnswerChoice"
   has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+    # result_hash = Hash.new(0)
+    # result_hash["Question"] = self.body
+    # self.answer_choices.includes(:responses).each do |choice|
+    #   result_hash[choice.body] = choice.responses.length
+    # end
+    # result_hash
+    self.answer_choices.where(answer_choices: { question_id: self.id }).left_outer_joins(:responses).group("answer_choices.body").select("responses.answer_id").count
+  end
 end
